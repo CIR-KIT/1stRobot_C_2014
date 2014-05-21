e69b13242fc3bb5b57853027602a35fb4ef4fdc8
Ryodo Tanaka <k104073r@mail.kyutech.jp>
2014-05-21 15:05:38 +0900

    20140405

--- SRC/main.c
+++ SRC/main.c
@@ -1,39 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-
+  //  unsigned char i=0;
   MVcmd JScmd;
-  EncCmd cnt;
   
   //constract JScmd
-  JScmd.gear  = FORWARD;
+  JScmd.gear = FORWARD;
   JScmd.speed = 0;
-  JScmd.dir   = KEEP;
-  JScmd.deg   = 0;
+  JScmd.dir =  KEEP;
+  JScmd.deg = 0;
   
-  //Open Joy Stick
-  //  SetJoystick();
-  //Open Arduino
-  // SetArduino(SEND_ID);
-  SetArduino(RECV_ID);
-
- 
-
-  //Main Roop
+  SetJoystick();
+  SetArduino(SEND_ID);
   while(1){
-    //GetJSinfo2MVcmd(&JScmd);
-    //Move(JScmd.gear, JScmd.speed, JScmd.dir);
-    ReadEncoder(&cnt);
-    printf("%d\t%d\t%d\n",cnt.cmd[0], cnt.cmd[1], cnt.data);
+    GetJSinfo2MVcmd(&JScmd);
+    Move(JScmd.gear, JScmd.speed, JScmd.dir);
   }
 
-  //Close Arduino
-  //CloseArduino(SEND_ID);
-  CloseArduino(RECV_ID);
-  //Close JoyStick
-  //CloseJoystick();
+  CloseArduino(SEND_ID);
+  CloseJoystick();
 
   return 0;
 }
